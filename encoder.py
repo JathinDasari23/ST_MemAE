1d0e9d223624704ad697fe05374baad7ac06a5fe
JathinDasari23 <dasarijathin3@gmail.com>
2025-07-04 14:13:15 +0530

    Create encoder.py

---
+++ encoder.py
@@ -0,0 +1,45 @@
+# model/encoder.py
+
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels=3):
+        super(Encoder, self).__init__()
+        self.enc1 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU()
+        )
+        self.pool1 = nn.MaxPool2d(2)
+        self.enc2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU()
+        )
+        self.pool2 = nn.MaxPool2d(2)
+        self.enc3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU()
+        )
+        self.pool3 = nn.MaxPool2d(2)
+        self.enc4 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU()
+        )
+    
+    def forward(self, x):
+        e1 = self.enc1(x)
+        p1 = self.pool1(e1)
+        e2 = self.enc2(p1)
+        p2 = self.pool2(e2)
+        e3 = self.enc3(p2)
+        p3 = self.pool3(e3)
+        e4 = self.enc4(p3)
+        return [e1, e2, e3, e4]
